8dcf361abe5c9bbc62c44bebb02623c64c40be5e
Fathiah <fathiahkazeem@gmail.com>
2025-05-21 20:45:54 +0100

    Initial commit

---
+++ my_imac/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';  // Your general app styles
+import BillionHarvestUpdates from './BillionHarvestUpdates';  // Import the banner component
+
+function App() {
+  return (
+    <div className="App">
+      <BillionHarvestUpdates />
+    </div>
+  );
+}
+
+export default App;
